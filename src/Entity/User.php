fa9dc9cb9fdccce7be97cf7a9586c96aa95e6654
Sam Wilson <sam@samwilson.id.au>
2020-05-03 10:21:06 +0800

    Add runtime cache of user IDs
    Bug: T251658

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,23 +11,23 @@ class User extends Model {
 	 */
 	protected $fillable = [ 'name' ];
 
+	/** @var int[] */
+	protected static $userIds;
+
 	public function admins() {
 		return $this->belongsToMany( Contest::class, 'admins' );
 	}
 
-// public function getIdFromUsername( $username ) {
-// if ( isset( $this->userIds[$username] ) ) {
-// return $this->userIds[$username];
-// }
-// // Create the user row if required.
-// $userCreate = $this->db->prepare( 'INSERT IGNORE INTO users SET name=:name' );
-// $userCreate->execute( [ 'name' => $username ] );
-// // Get its ID.
-// $userFind = $this->db->prepare( 'SELECT id FROM users WHERE name=:name' );
-// $userFind->execute( [ 'name' => $username ] );
-// $this->userIds[$username] = $userFind->fetchColumn();
-//
-// return $this->userIds[$username];
-// }
+	/**
+	 * @param string $username
+	 * @return int
+	 */
+	public static function getIdFromUsername( string $username ) {
+		if ( isset( static::$userIds[$username] ) ) {
+			return static::$userIds[$username];
+		}
+		static::$userIds[$username] = static::firstOrCreate( [ 'name' => $username ] )->id;
+		return static::$userIds[$username];
+	}
 
 }
